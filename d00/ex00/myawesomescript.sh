a1dff86b90b8de7f0a599f950e239ccdfa48af30
Bruno Shiroma <bshiroma@c2r10p4.42sp.org.br>
2023-08-01 10:29:21 -0300

    first

---
+++ d00/ex00/myawesomescript.sh
@@ -0,0 +1,7 @@
+#Esta linha é conhecida como "shebang" e indica qual interpretador deve ser usado para executar o script. Neste caso, estamos usando /bin/sh, que é um interpretador padrão de shell.
+#!/bin/sh
+
+#curl -sI "$1": O comando "curl" é usado para fazer uma requisição HTTP para o link bit.ly passado como o primeiro argumento para o script ($1). A opção -s faz o curl ser executado em modo silencioso, sem exibir informações de progresso. A opção -I diz ao curl para fazer uma solicitação HEAD, que retornará apenas os cabeçalhos da resposta HTTP, não o corpo da página.
+#grep -i "location:": O comando "grep" é usado para filtrar as linhas de saída do curl e encontrar aquela que contém a palavra "Location:". A opção -i torna a pesquisa case-insensitive, para que não importe se a palavra está em maiúsculas ou minúsculas.
+#cut -d' ' -f2: O comando "cut" é usado para extrair a segunda coluna de texto da saída do comando anterior, utilizando o espaço como delimitador. Neste caso, a segunda coluna contém a URL de redirecionamento que desejamos obter.          
+curl -sI $1 | grep -i "location: " | cut -d" " -f2
