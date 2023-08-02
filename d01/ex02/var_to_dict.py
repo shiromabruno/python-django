4425708c238603616573ad83ac0e1f0e94469bd2
Bruno Shiroma <bshiroma@c2r10p4.42sp.org.br>
2023-08-02 10:44:10 -0300

    atualizacao

--- d01/ex02/var_to_dict.py
+++ d01/ex02/var_to_dict.py
@@ -1,6 +1,41 @@
 
-def main():
-    return 5
+def main(lista_tupla):
+    
+    dicionario_novo = {}
+
+    #('Hendrix' , '1942')
+    for valor, chave in lista_tupla:
+        dicionario_novo[chave] = valor
+
+    for chave in dicionario_novo.keys():
+        print(f'{chave} : {dicionario_novo[chave]}')
 
 if __name__ == '__main__':
-    main()
+    d = [
+('Hendrix' , '1942'),
+('Allman' , '1946'),
+('King'
+, '1925'),
+('Clapton' , '1945'),
+('Johnson' , '1911'),
+('Berry'
+, '1926'),
+('Vaughan' , '1954'),
+('Cooder' , '1947'),
+('Page'
+, '1944'),
+('Richards' , '1943'),
+('Hammett' , '1962'),
+('Cobain' , '1967'),
+('Garcia' , '1942'),
+('Beck'
+, '1944'),
+('Santana' , '1947'),
+('Ramone' , '1948'),
+('White'
+, '1975'),
+('Frusciante', '1970'),
+('Thompson' , '1949'),
+('Burton' , '1939')
+]
+    main(d)
---
+++ d01/ex03/capital_city.py
@@ -0,0 +1,34 @@
+
+import sys
+
+def main(find_state):
+    
+    states = {
+        "Oregon" : "OR",
+        "Alabama" : "AL",
+        "New Jersey": "NJ",
+        "Colorado" : "CO"
+   }
+
+    capital_cities = {
+        "OR": "Salem",
+        "AL": "Montgomery",
+        "NJ": "Trenton",
+        "CO": "Denver"
+    }
+
+    if find_state in states:
+        capital_sigla = states[find_state]
+        if capital_sigla in capital_cities:
+            print (capital_cities[capital_sigla])
+        else:
+            print ("Unknown state")
+        
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 2:
+        sys.exit()
+
+arg1 = sys.argv[1]
+main(arg1)
